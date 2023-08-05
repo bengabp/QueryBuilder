5f1034173342350d4e66f9f19628e013bc11a1e5
bengabp <bengabdev4@gmail.com>
2023-08-05 11:26:42 +0100

    Implemented /settings endpint

--- backend/main.py
+++ backend/main.py
@@ -1,82 +1,60 @@
-import os
-import sys
-from fastapi import FastAPI, Query, status, HTTPException, Request, Body
+from fastapi import FastAPI, Query, status, Request
 from fastapi.middleware.cors import CORSMiddleware
-from config import logger
-from db import db
-import pandas
-from typing import Dict, List
-from response_models import CompletionsResponse, CompaniesSearchResult, SearchRequest
+
+import time
 from constants import field_mappings
+from db import db
+from response_models import CompletionsResponse, CompaniesSearchResult, SearchRequest, SettingsResponse
 from utils.completions import get_search_suggestions
 
 app = FastAPI()
 
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins = ["*"],
-	allow_credentials = True,
-	allow_methods = ["*"],
-	allow_headers = ["*"]
+	allow_origins=["*"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"]
 )
 
 
 @app.get(
 	"/completions",
-	name = "completions",
-	tags = ["Completions"],
-	summary = "Get completions",
-	description = "Get completions for search fields",
-	responses = {
+	name="completions",
+	tags=["Completions"],
+	summary="Get completions",
+	description="Get completions for search fields",
+	responses={
 		status.HTTP_200_OK: {
 			"model": CompletionsResponse
 		}
 	}
 )
 def get_completions(request: Request,
-					q = Query(
-						default = "",
-						description = "String to search for, leaving this field empty will return some random results..",
+					q=Query(
+						default="",
+						description="String to search for, leaving this field empty will return some random results..",
 					),
-					field_path = Query(
-						default = "name",
-						description = "The path to the field to get completions for",
-						min_length = 3,
-						example = "fundings.items.amount"
+					field_path=Query(
+						default="name",
+						description="The path to the field to get completions for",
+						min_length=3,
+						example="fundings.items.amount"
 					)
 					):
 	""" Get completions for search query using mongodb aggregate queries/pipelines """
 	corresponding_field = field_mappings[field_path]["dKey"]
-	completions = get_search_suggestions(corresponding_field, q, limit = 20)
-	return CompletionsResponse(completions = completions)
-
-
-@app.get(
-	"/companies_first",
-	name = "companies_first",
-	summary = "Get first x companies when page loads",
-	responses = {
-		status.HTTP_200_OK: {
-			"model": CompaniesSearchResult
-		}
-	}
-)
-def get_initial_load_data(request: Request):
-	result = db.companies.find({}).limit(10)
-	companies = []
-	for company in result:
-		company.pop("_id", None)
-		companies.append(company)
-	return CompaniesSearchResult(matches = companies, total_matches = len(companies))
+	completions = get_search_suggestions(corresponding_field, q, limit=20)
+	return CompletionsResponse(completions=completions)
 
 
 @app.post(
 	"/search",
-	name = "companies_search",
-	tags = ["Search"],
-	summary = "Companies Search",
-	description = "Companies search endpoint for react frontend",
-	responses = {
+	name="companies_search",
+	tags=["Search"],
+	summary="Companies Search",
+	description="Companies search endpoint for react frontend",
+	responses={
 		status.HTTP_200_OK: {
 			"model": CompaniesSearchResult
 		}
@@ -88,9 +66,9 @@ def companies_search(request: Request, search_filters: SearchRequest):
 	for search_filter in search_filters.filters:
 		query = ".".join([*search_filter.parents, search_filter.data_key])
 		query = field_mappings[query]["dKey"]
-		
+
 		stage = {}
-		
+
 		if search_filter.d_type == "string":
 			if search_filter.current_option == "includes_any":
 				stage = {"$match": {query: {"$in": search_filter.values}}}
@@ -121,7 +99,7 @@ def companies_search(request: Request, search_filters: SearchRequest):
 					]}}
 			elif search_filter.current_option == "equals":
 				stage = {"$match": {query: search_filter.values[0]}}
-		
+
 		elif search_filter.d_type == "number":
 			if search_filter.current_option == "between":
 				stage = {"$match": {query: {"$gte": search_filter.values[0], "$lte": search_filter.values[1]}}}
@@ -150,17 +128,38 @@ def companies_search(request: Request, search_filters: SearchRequest):
 					]}}
 		elif search_filter.d_type == "date":
 			pass
-		
+
 		else:
 			print("unsupported data type !")
-		
+
 		pipeline.append(stage)
-	
+
 	results = db.companies.aggregate(pipeline)
 	matches = []
 	for record in results[:1000]:
 		record.pop("_id", None)
 		matches.append(record)
-	
+
 	total_matches = len(matches)
-	return CompaniesSearchResult(matches = matches, total_matches = total_matches)
+	return CompaniesSearchResult(matches=matches, total_matches=total_matches)
+
+@app.get("/settings")
+def get_settings(request: Request, include_companies:bool = Query(alias="includeCompanies", default = True)):
+	companies_result = {}
+	response = SettingsResponse(companies = companies_result)
+
+	if include_companies:
+		# Get first 5 companies:
+		search_result = [company for company in db.companies.find({})[:5]]
+		companies = []
+		for company in search_result:
+			company.pop("_id", None)
+			companies.append(company)
+
+		total_results = db.companies.count_documents({})
+		response.companies = CompaniesSearchResult(
+			results = companies,
+			totalResults = total_results
+		)
+
+	return response
