1acc363a7f2a3ce3c2be0516b8b66ab29ccc4ef5
bengabp <bengabdev4@gmail.com>
2023-10-18 23:57:36 +0000

    Increased page size

--- frontend/src/components/SearchResultsTable.jsx
+++ frontend/src/components/SearchResultsTable.jsx
@@ -55,7 +55,7 @@ export default function SearchResultsTable(props) {
     },
     {
       field: "type",
-      headerName: "type",
+      headerName: "Type",
       width: 80,
       editable: false
     },
